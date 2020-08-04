8c93ff445ba7321065e17b1c3858373d9b6534a4
luisgarciamarcelo <67678570+luisgarciamarcelo@users.noreply.github.com>
2020-08-04 17:09:31 -0500

    Add files via upload

---
+++ database/migrations/2019_05_30_145519_create_columns_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateColumnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('columns', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('column');
+            $table->string('lado A');
+            $table->string('lado B');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('columns');
+    }
+}
